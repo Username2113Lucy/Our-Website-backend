6068eb7325371c6497b93954f6c63d2d99e88510
Username2113Lucy <dharshana2113@gmail.com>
2025-10-09 12:23:51 +0530

    "added admin panal cors"

--- server.js
+++ server.js
@@ -18,6 +18,7 @@ app.use(cors({
   origin: [
     'https://www.vetriantechnologysolutions.in',
     'https://vetriantechnologysolutions.in',
+    'https://our-website-admin.vercel.app',
     'http://localhost:5173',
     'http://localhost:5174', 
     'http://localhost:3000',
